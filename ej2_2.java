271ccb607b1dd3febfa0f7085df20ad94861f032
niidea <101263223+niidea1650@users.noreply.github.com>
2023-03-29 10:50:56 -0300

    29/3 act:

---
+++ ej2_2.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ej2_2 {
+    public static void main(String[] args) {
+        Scanner palabras = new Scanner(System.in);
+        int t_array;
+        char letra;
+        String palabra = " ";
+        ArrayList<String> cadena = new ArrayList<String>();
+        System.out.println("ingresar cantidad de palabras:");
+        t_array = palabras.nextInt();
+        for (int i = 0; i < t_array; i++) {
+            System.out.println("ingresa la palabra " + (i + 1));
+            palabra = palabras.next();
+            cadena.add(palabra);
+        }
+        System.out.println("Ingrese la letra para filtrar:");
+        letra = palabras.next().charAt(0);
+        System.out.println("Las cadenas que empiezan con " + letra + " son:");
+
+        for (String palabraActual : cadena) {
+            if (palabraActual.charAt(0) != palabra.charAt(0)) {
+                System.out.println(palabraActual);
+            }
+        }
+    }
+}
+
